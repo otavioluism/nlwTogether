cf7f2ba2e5404706aca5c4a8e3f15a7055ada2f6
Otávio Luis Martins <otavioluism97@hotmail.com>
2021-07-02 18:16:38 -0300

    inserido tabela de elogios com regra de negocio

---
+++ src/entities/Compliment.ts
@@ -0,0 +1,57 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import Tag from './Tag';
+import User from './User';
+
+
+@Entity('compliments')
+class Compliment { 
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_sender: string;
+
+  @JoinColumn({ name: "user_sender" })
+  @ManyToOne(() => User)
+  UserSender: User;
+
+
+
+
+  @Column()
+  user_receiver: String;
+
+  @JoinColumn({ name: "user_receiver"})
+  @ManyToOne(() => User)
+  userReceiver: User;
+
+
+
+  @Column()
+  tag_id: string;
+
+  // Referenciando o id da tag_id da tabela compliments 
+  // com a relação na tabela Tag
+  @JoinColumn({ name: 'tag_id'})
+  @ManyToOne(() => Tag) // Muitos elogios para uma mesma tag
+  tag: Tag;
+
+
+
+  @Column()
+  message:string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor () { 
+    if (!this.id) { 
+      this.id = uuid();
+    }
+  }
+
+}
+
+export default Compliment;
--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,21 @@ import { Router } from 'express';
 import CreateUserController from './controllers/CreateUserController';
 import CreateTagController from './controllers/CreateTagController'; 
 import AuthenticateUserController from './controllers/AuthenticateUserController';
+import CreateComplimentController from './controllers/CreateComplimentController';
 
 import ensureAdmin from './middlewares/ensureAdmin';
-import AuthenticateUserService from './services/AuthenticateUserService';
+
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
 router.post('/login', authenticateUserController.handle);
+router.post('/compliment', createComplimentController.handle);
 
 export default router;
